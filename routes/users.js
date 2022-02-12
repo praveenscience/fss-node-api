9bb90cd11b1f810019593ef4dfd8b68fa9c19ce8
Praveen Kumar <praveenscience@gmail.com>
2022-02-12 07:17:27 +0000

    Handled the non-existent users.

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,11 @@ app.get("/", (req, res) => {
   res.json(Users);
 });
 app.get("/:User", (req, res) => {
-  res.json(Users[req.params.User]);
+  if (Users[req.params.User]) {
+    res.json(Users[req.params.User]);
+  } else {
+    res.status(404).json("Dei poda!");
+  }
 });
 
 module.exports = app;
