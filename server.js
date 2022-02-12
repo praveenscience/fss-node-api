1c8093e914ede2412b36bb122031f6f953ea74dc
Praveen Kumar <praveenscience@gmail.com>
2022-02-12 07:02:24 +0000

    Created a root end point.

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+app.get("/", (req, res) => {
+  res.json("Hello FSS! 👋🏻");
+});
+
 app.listen(port, () => {
   console.log(`Server started in port ${port}.`);
 });
