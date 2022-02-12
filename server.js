1c874ea3a9f820d242694d47d97fda11d6ca010b
Praveen Kumar <praveenscience@gmail.com>
2022-02-12 07:15:38 +0000

    Imported the users route.

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const Users = require("./routes/users");
 
 app.get("/", (req, res) => {
   res.json("Hello FSS! 👋🏻");
 });
 
-app.get("/:User", (req, res) => {
-  res.json({
-    Message: `Hello ${req.params.User}! 👋🏻`,
-    Params: req.params,
-    QueryString: req.query
-  });
-});
+app.use("/users", Users);
 
 app.listen(port, () => {
   console.log(`Server started in port ${port}.`);
