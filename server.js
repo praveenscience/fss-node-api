c00d940b8d63883ab1f97199b890446da26412ef
Praveen Kumar <praveenscience@gmail.com>
2022-02-12 07:05:07 +0000

    Added more paths.

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ app.get("/", (req, res) => {
   res.json("Hello FSS! 👋🏻");
 });
 
+app.get("/chintan357", (req, res) => {
+  res.json("Hello Chintan! 👋🏻");
+});
+app.get("/loki", (req, res) => {
+  res.json("Hello Lokesh V! 👋🏻");
+});
+
 app.listen(port, () => {
   console.log(`Server started in port ${port}.`);
 });
