41b40f1258584919c50c51762485690b77ac6b4e
Praveen Kumar <praveenscience@gmail.com>
2022-02-12 07:11:46 +0000

    Created a User Data structure.

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const app = express.Router();
 
+const Users = {
+  loki: "Lokesh Venkatesan",
+  praveen: "Praveen Kumar Purushothaman",
+  sridhu: "Sridhar Kasukurthy",
+  jo: "Joshua Poddoku",
+  ved001: "Vedant Khairnar"
+};
+
 module.exports = app;
